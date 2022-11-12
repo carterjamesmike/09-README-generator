2ba09a206f2c790a51ea67fbdaaa967b74a164a8
James Carter <carterjamesmike@gmail.com>
2022-11-12 10:12:25 -0600

    First commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,49 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ])
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
+
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
