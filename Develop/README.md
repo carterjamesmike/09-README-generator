f5f1ccac131832731f1956ee10ebb7ce1e0095b1
James Carter <carterjamesmike@gmail.com>
2022-11-12 13:20:48 -0600

    Basic framework

---
+++ Develop/README.md
@@ -0,0 +1,34 @@
+# Generate a ReadME
+
+## Description
+This generates a readme
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Licence](#licence)
+4. [Contributors](#contributors)
+5. [Test](#test)
+6. [Questions](#questions)
+
+
+## Installation <div id='installation'/>
+To install, run this command:
+>undefined
+
+## Usage <div id='usage'/>
+Nothing
+
+## Licence <div id='licence'/>
+This project is run under the undefined licensure
+
+## Contributors <div id='contributors'/>
+The contributors are: N/A
+
+
+## Test <div id='test'/>
+For tests, run this command:
+>node index.js
+
+## Questions <div id='questions'/>
+For quetions, please email at carterjamesmike@gmail.com or visit the repo at carterjamesmike
--- Develop/index.js
+++ Develop/index.js
@@ -3,53 +3,102 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-inquirer
+const gernerateReadme = ({projectTitle, description, install, usage, licence, contributors,tests, email, github }) =>
+  `# ${projectTitle}
+
+## Description
+${description}
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Licence](#licence)
+4. [Contributors](#contributors)
+5. [Test](#test)
+6. [Questions](#questions)
+
+
+## Installation <div id='installation'/>
+To install, run this command:
+>${install}
+
+## Usage <div id='usage'/>
+${usage}
+
+## Licence <div id='licence'/>
+This project is run under the ${licence} licensure
+
+## Contributors <div id='contributors'/>
+The contributors are: ${contributors}
+
+
+## Test <div id='test'/>
+For tests, run this command:
+>${tests}
+
+## Questions <div id='questions'/>
+For quetions, please email at ${email} or visit the repo at ${github}`;
+
+
+// TODO: Create a function to write README file
+function writeToFile(answers) {
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer
   .prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
-    },
-    {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
-    },
-    {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
-    },
-    {
-      type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username',
+      message: 'What is your github user name?',
     },
     {
       type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
+      name: 'email',
+      message: 'What is your email?',
+    },
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'What is the name of the project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Write a short description about the project.',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What license is this project under?',
+      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What command is needed to run tests?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What does the user need to know to use the repo?',
+    },
+    {
+      type: 'input',
+      name: 'contributors',
+      message: 'Who are the contributors?',
     },
   ])
   .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+    const readmeContent = gernerateReadme(answers);
 
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
+    fs.writeFile('README.md', readmeContent, (err) =>
+      err ? console.log(err) : console.log('Created readme file')
     );
-  });
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+  }); 
+}
 
 // Function call to initialize app
 init();
